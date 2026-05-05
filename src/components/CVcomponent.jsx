35f133d52f1d63a168612a3482eb2f2741f4a119
Damian Wujec <wujecdamian12@gmail.com>
2026-05-05 21:04:48 +0200

    feat: add editMode(state) handling button,function and call components with editmode prop

--- src/components/CVcomponent.jsx
+++ src/components/CVcomponent.jsx
@@ -1,15 +1,24 @@
-import CVheading from "./components/CVsubComponents/CVheading.jsx";
-import CVgeneral from "./components/CVsubComponents/CVgeneral.jsx";
-import CVeducation from "./components/CVsubComponents/CVeducation.jsx";
-import CVexperience from "./components/CVsubComponents/CVexperience.jsx";
+import CVheading from "./CVsubComponents/CVheading.jsx";
+import CVgeneral from "./CVsubComponents/CVgeneral.jsx";
+import CVeducation from "./CVsubComponents/CVeducation.jsx";
+import CVexperience from "./CVsubComponents/CVexperience.jsx";
+import { useState } from "react";
 
 export default function CVcomponent() {
+  const [editMode, setEditMode] = useState(true);
+
+  function handleEditMode() {
+    setEditMode(!editMode);
+    console.log(editMode);
+  }
+
   return (
     <>
       <CVheading></CVheading>
-      <CVgeneral></CVgeneral>
-      <CVeducation></CVeducation>
-      <CVexperience></CVexperience>
+      <CVgeneral editMode={editMode}></CVgeneral>
+      <CVeducation editMode={editMode}></CVeducation>
+      <CVexperience editMode={editMode}></CVexperience>
+      <button onClick={handleEditMode}>{editMode ? "Submit" : "Edit"}</button>
     </>
   );
 }
