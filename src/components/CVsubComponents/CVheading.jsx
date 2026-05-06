c2870be2cc61399ad690f9ee05a38e3d6710b519
Damian Wujec <wujecdamian12@gmail.com>
2026-05-06 20:11:54 +0200

    refactor: change order of editMode (t/f) h1's

--- src/components/CVsubComponents/CVheading.jsx
+++ src/components/CVsubComponents/CVheading.jsx
@@ -2,11 +2,11 @@ export default function CVheading({ editMode }) {
   return (
     <>
       {editMode ? (
-        <h1>CV Creator</h1>
-      ) : (
         <h1>
           CV Creator <i>(edit mode)</i>
         </h1>
+      ) : (
+        <h1>CV Creator</h1>
       )}
     </>
   );
