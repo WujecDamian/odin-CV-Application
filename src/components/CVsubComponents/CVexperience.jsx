c20d55c6f8f6c9ed3064191127a09b56928f2709
Damian Wujec <wujecdamian12@gmail.com>
2026-05-07 16:47:56 +0200

    function: make component reusable with proper editMode handling

--- src/components/CVsubComponents/CVexperience.jsx
+++ src/components/CVsubComponents/CVexperience.jsx
@@ -1,93 +1,62 @@
-import { createElement, useState } from "react";
-import React from "react";
+import { useState } from "react";
 
-export default function CVexperience({ editMode }) {
-  const [modulesArray, setModulesArray] = useState([
-    React.createElement(ExperienceModule),
-  ]);
+export default function CVeducation({ editMode }) {
+  const [company, setCompany] = useState("");
+  const [positionTitle, setPositionTitle] = useState("");
+  const [date, setDate] = useState("");
 
-  function ExperienceModule() {
-    const [company, setCompany] = useState("");
-    const [positionTitle, setPositionTitle] = useState("");
-    const [date, setDate] = useState("");
-
-    //K.I.S.S. - Keep It Simple Stupid
-    function handleChangeCompany(e) {
-      setCompany(e.target.value);
-    }
-    function handleChangePositionTitle(e) {
-      setPositionTitle(e.target.value);
-    }
-    function handleChangeDate(e) {
-      setDate(e.target.value);
-    }
-    if (editMode === true) {
-      return (
-        <section className="Experience_Module">
-          <div className="input">
-            <label htmlFor="company">company: </label>
-            <input
-              type="text"
-              name="company"
-              id="company"
-              value={company}
-              onChange={handleChangeCompany}
-            />
-          </div>
-          <div className="input">
-            <label htmlFor="positionTitle">positionTitle: </label>
-            <input
-              type="text"
-              name="positionTitle"
-              id="positionTitle"
-              value={positionTitle}
-              onChange={handleChangePositionTitle}
-            />
-          </div>
-          <div className="input">
-            <label htmlFor="date">date: </label>
-            <input
-              type="tel"
-              name="date"
-              id="date"
-              value={date}
-              onChange={handleChangeDate}
-            />
-          </div>
-        </section>
-      );
-    } else {
-      return (
-        <section className="experience_Module">
-          <h3>Company: {company}</h3>
-          <h3>Position title: {positionTitle}</h3>
-          <h3>Date: {date}</h3>
-        </section>
-      );
-    }
+  //K.I.S.S. - Keep It Simple Stupid
+  function handleChangeCompany(e) {
+    setCompany(e.target.value);
   }
-
-  function addModule() {
-    console.log(modulesArray.length);
-    let newModulesArray = modulesArray.slice();
-    const eduModule = React.createElement(ExperienceModule);
-    newModulesArray.push(eduModule);
-    setModulesArray(newModulesArray);
-    RenderModules();
+  function handleChangePositionTitle(e) {
+    setPositionTitle(e.target.value);
   }
-  function RenderModules() {
-    return createElement(
-      "section",
-      { className: "ExperienceModules" },
-      ...modulesArray,
+  function handleChangeDate(e) {
+    setDate(e.target.value);
+  }
+  if (editMode === true) {
+    return (
+      <section className="experience_Module">
+        <div className="input">
+          <label htmlFor="company">Company: </label>
+          <input
+            type="text"
+            name="company"
+            id="company"
+            value={company}
+            onChange={handleChangeCompany}
+          />
+        </div>
+        <div className="input">
+          <label htmlFor="titleOfStudy">Position title: </label>
+          <input
+            type="text"
+            name="titleOfStudy"
+            id="titleOfStudy"
+            value={positionTitle}
+            onChange={handleChangePositionTitle}
+          />
+        </div>
+        <div className="input">
+          <label htmlFor="date">Date: </label>
+          <input
+            type="tel"
+            name="date"
+            id="date"
+            value={date}
+            onChange={handleChangeDate}
+          />
+        </div>
+      </section>
+    );
+  } else {
+    return (
+      <section className="education_Module">
+        <h3>Company: {company}</h3>
+        <h3>Position title: {positionTitle}</h3>
+        <h3>Date: {date}</h3>
+      </section>
     );
   }
-
-  return (
-    <section className="inputs">
-      Experience
-      <RenderModules />
-      <button onClick={addModule}>+</button>
-    </section>
-  );
 }
