0fd11cbdbfdc246a48d50d77245cba8f7d3da7fa
Damian Wujec <wujecdamian12@gmail.com>
2026-05-07 21:17:52 +0200

    style: add className to CV heading for consistent styling

--- src/components/CVsubComponents/CVheading.jsx
+++ src/components/CVsubComponents/CVheading.jsx
@@ -2,11 +2,11 @@ export default function CVheading({ editMode }) {
   return (
     <>
       {editMode ? (
-        <h1>
-          CV Creator <i>(edit mode)</i>
+        <h1 className="CV_heading">
+          CV Creator <i className="edit">(edit mode)</i>
         </h1>
       ) : (
-        <h1>CV Creator</h1>
+        <h1 className="CV_heading">CV Creator</h1>
       )}
     </>
   );
