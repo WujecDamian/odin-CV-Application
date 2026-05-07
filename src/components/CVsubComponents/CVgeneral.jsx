5ca65cde07dabb3a12e48391b283040b9bcfb477
Damian Wujec <wujecdamian12@gmail.com>
2026-05-07 20:28:47 +0200

    feat: add module section to general inputs

--- src/components/CVsubComponents/CVgeneral.jsx
+++ src/components/CVsubComponents/CVgeneral.jsx
@@ -19,35 +19,37 @@ export default function CVgeneral({ editMode }) {
     return (
       <section className="CV_general">
         <h2>General</h2>
-        <div className="input">
-          <label htmlFor="Name">Name: </label>
-          <input
-            type="text"
-            name="Name"
-            id="name"
-            value={name}
-            onChange={(e) => handleChange("name", e)}
-          />
-        </div>
-        <div className="input">
-          <label htmlFor="Email">Email: </label>
-          <input
-            type="email"
-            name="Email"
-            id="name"
-            value={email}
-            onChange={(e) => handleChange("email", e)}
-          />
-        </div>
-        <div className="input">
-          <label htmlFor="Phone">Phone: </label>
-          <input
-            type="tel"
-            name="Phone"
-            id="name"
-            value={phone}
-            onChange={(e) => handleChange("phone", e)}
-          />
+        <div className="general_Module">
+          <div className="input">
+            <label htmlFor="Name">Name: </label>
+            <input
+              type="text"
+              name="Name"
+              id="name"
+              value={name}
+              onChange={(e) => handleChange("name", e)}
+            />
+          </div>
+          <div className="input">
+            <label htmlFor="Email">Email: </label>
+            <input
+              type="email"
+              name="Email"
+              id="name"
+              value={email}
+              onChange={(e) => handleChange("email", e)}
+            />
+          </div>
+          <div className="input">
+            <label htmlFor="Phone">Phone: </label>
+            <input
+              type="tel"
+              name="Phone"
+              id="name"
+              value={phone}
+              onChange={(e) => handleChange("phone", e)}
+            />
+          </div>
         </div>
       </section>
     );
